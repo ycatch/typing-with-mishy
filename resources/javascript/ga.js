80bfd6398f3d0c968e489bf801f0ed0263412de8
Yutaka Catch <yyycatch@gmail.com>
2022-07-05 01:27:51 +0900

    change GA

--- resources/javascript/ga.js
+++ resources/javascript/ga.js
@@ -5,6 +5,6 @@ if (window.location.hostname == 'sethclydesdale.github.io') {
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-180306096-1', 'auto');
+  ga('create', 'G-QZ3HJCZ49B', 'auto');
   ga('send', 'pageview');
 }
